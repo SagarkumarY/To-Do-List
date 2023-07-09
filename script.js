f677578d8200b6a9ff2292384b733d420c9aa377
Sagar <131738417+RefixSagar@users.noreply.github.com>
2023-07-09 23:53:43 +0530

    comment code remove

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-// const liList = document.querySelectorAll("li");
-
-// liList.forEach((li) => {
-//   li.addEventListener("click", () => {
-//     li.classList.toggle("checked");
-//   });
-// });
-
-
-// const inputBox = document.getElementById("input-box");
-
-// const btn = document.getElementById("btn");
-
-
-// const ul = document.getElementById('ul-container')
-// console.log(ul)
-
-
-// function addtask() {
-//   if (inputBox.value === "") {
-//     alert("ddddddddd")
-//   } else {
-//     let li = document.createElement("li");
-//     li.innerHTML = inputBox.value;
-//     ul.appendChild(li);
-
-//     let span = document.createElement("span")
-//     span.innerHTML = "\u00d7"
-//     li.appendChild(span)
-//   }
-
-//   inputBox.value = " ";
-// }
-
-
-// ul.addEventListener("click",(e)=>{
-//     if(e.target.tagName === 'Li'){
-//         e.target.classList.toggle("checked")
-//     } else if(e.target.tagName === "SPAN"){
-//         e.target.parentElement.remove();
-//     }
-// },false)
 
 
 
@@ -47,32 +5,9 @@
 
 
 
-// const inputBox = document.getElementById("input-box");
-// const ulContainer = document.getElementById("ul-container");
 
-// function addtask() {
-//   if (inputBox.value.trim() === "") {
-//     alert("Please enter a task!");
-//   } else {
-//     const li = document.createElement("li");
-//     li.textContent = inputBox.value;
-//     ulContainer.appendChild(li);
 
-//     const span = document.createElement("span");
-//     span.textContent = "\u00d7";
-//     li.appendChild(span);
 
-//     inputBox.value = "";
-//   }
-// }
-
-// ulContainer.addEventListener("click", (e) => {
-//   if (e.target.tagName === "LI") {
-//     e.target.classList.toggle("checked");
-//   } else if (e.target.tagName === "SPAN") {
-//     e.target.parentElement.remove();
-//   }
-// });
 
 
 const inputBox = document.getElementById("input-box");
